e87299469f746d3867387b71d04477e67b746b8f
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2022-11-02 00:54:35 +0500

    news bo`limini post fubksiyasi ishladi

--- src/views/News/create.vue
+++ src/views/News/create.vue
@@ -1,48 +1,52 @@
 <template>
 
     <Layout>
-    <form @submit.prevent="postData">
-        <div class="my-8 mx-40 rounded-xl drop-shadow-md pt-20 pb-20 bg-white">
-            <div class="flex justify-evenly w-full">
-                <div class="space-y-6 news">
-                    <h1 class="news-info">yangilik o'zbek tilida qo'shish</h1>
-                    <hr />
-                    <InputComponent required class="w-full" name="Yangilik nomi uz" v-model="yangilik.title_uz" />
-                    <TextAreaComponents required class="w-full" v-model="yangilik.body_uz" name="Yangilik matni uz" />
-    
-                    <h1 class="news-info">yangilik rus tilida qo'shish</h1>
-                    <hr />
-                    <InputComponent required class="w-full" name="Yangilik nomi ru" v-model="yangilik.title_ru" />
-                    <TextAreaComponents required class="w-full" v-model="yangilik.body_ru" name="Yangilik matni ru" />
-    
-                    <h1 class="news-info">yangilik ingliz tilida qo'shish</h1>
-                    <hr />
-                    <InputComponent required class="w-full" name="Yangilik nomi eng" v-model="yangilik.title_eng" />
-                    <TextAreaComponents required class="w-full" v-model="yangilik.body_eng" name="Yangilik matni eng" />
-                    <div class="news-date ">
-    
-                        <InputComponent required class="w-full" name="yangilik kiritilgan vaqti" type="date" v-model="date" />
-    
-                        <InputComponent required class="w-full" type="file" name="yangilik rasmi" v-model="yangilik.newsImg" />
+        <form @submit.prevent="postData">
+            <div class="my-8 mx-40 rounded-xl drop-shadow-md pt-20 pb-20 bg-white">
+                <div class="flex justify-evenly w-full">
+                    <div class="space-y-6 news">
+                        <h1 class="news-info">yangilik o'zbek tilida qo'shish</h1>
+                        <hr />
+                        <InputComponent required class="w-full" name="Yangilik nomi uz" v-model="yangilik.title_uz" />
+                        <TextAreaComponents required class="w-full" v-model="yangilik.body_uz"
+                            name="Yangilik matni uz" />
 
-    
+                        <h1 class="news-info">yangilik rus tilida qo'shish</h1>
+                        <hr />
+                        <InputComponent required class="w-full" name="Yangilik nomi ru" v-model="yangilik.title_ru" />
+                        <TextAreaComponents required class="w-full" v-model="yangilik.body_ru"
+                            name="Yangilik matni ru" />
+
+                        <h1 class="news-info">yangilik ingliz tilida qo'shish</h1>
+                        <hr />
+                        <InputComponent required class="w-full" name="Yangilik nomi eng" v-model="yangilik.title_eng" />
+                        <TextAreaComponents required class="w-full" v-model="yangilik.body_eng"
+                            name="Yangilik matni eng" />
+                        <div class="news-date ">
+
+                            <InputComponent required class="w-full" name="yangilik kiritilgan vaqti" type="date"
+                                v-model="date" />
+                            <InputComponent required class="w-full" name="Rasm yuklang" type="file"
+                                v-model="date" @change="onFileUpload" />
+                            <!-- <input type="file" @change=""> -->
+
+                        </div>
                     </div>
+
                 </div>
-    
-            </div>
-            <div class="news-btn">
-    
-    
-                <button type="submit"
-                    class="   bg-blue-800 text-white font-bold px-7 py-4 rounded-md cursor-pointer active:bg-blue-500">yuklash
-                </button>
-                <RouterLink to="/news">
+                <div class="news-btn">
+
+
                     <button type="submit"
-                        class="  bg-red-500 text-white font-bold px-7 py-4 rounded-md cursor-pointer active:bg-red-400">Ortga</button>
-                </RouterLink>
+                        class="   bg-blue-800 text-white font-bold px-7 py-4 rounded-md cursor-pointer active:bg-blue-500">yuklash
+                    </button>
+                    <RouterLink to="/news">
+                        <button type="submit"
+                            class="  bg-red-500 text-white font-bold px-7 py-4 rounded-md cursor-pointer active:bg-red-400">Ortga</button>
+                    </RouterLink>
+                </div>
             </div>
-        </div>
-    </form>
+        </form>
     </Layout>
 </template>
 
@@ -66,22 +70,30 @@ export default {
                 body_uz: "",
                 body_ru: "",
                 body_eng: "",
-                newsImg:""
+                date: "",
+                img: null
             }
         }
     },
     methods: {
+        onFileUpload(event) {
+            this.yangilik.img = event.target.files[0]
+        },
+
+
         postData() {
-            http.post('/news/add',
-                {
-                    title_uz: this.yangilik.title_uz,
-                    title_ru: this.yangilik.title_ru,
-                    title_en: this.yangilik.title_eng,
-                    body_uz: this.yangilik.body_uz,
-                    body_ru: this.yangilik.body_ru,
-                    body_en: this.yangilik.body_eng,
-                    photo: this.newsImg,
-                }
+            const formData = new FormData()
+
+            formData.append("title_uz", this.yangilik.title_uz)
+            formData.append("title_ru", this.yangilik.title_ru)
+            formData.append("title_en", this.yangilik.title_eng)
+            formData.append("body_uz", this.yangilik.body_uz)
+            formData.append("body_ru", this.yangilik.body_ru)
+            formData.append("body_en", this.yangilik.body_eng)
+            formData.append("date", this.yangilik.date)
+            formData.append("photo", this.yangilik.img, this.yangilik.img.name)
+            console.log(this.yangilik.img)
+            http.post('/news/add', formData
             ).then((res) => {
                 this.xabar()
                 console.log(res)
@@ -100,7 +112,7 @@ export default {
 </script>
 
 <style>
-.news{
+.news {
     width: 50vw;
     margin: 0 50px;
 }
@@ -122,7 +134,6 @@ export default {
 .news-btn {
     display: flex;
     margin: 20px 50px;
-
     gap: 15px;
 
 }
