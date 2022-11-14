872d75f45bb596c196d5a9cddb4c7f0479786c71
bekzodjonganiyev <bekzodganiyev471@gmail.com>
2022-11-14 16:38:16 +0500

    wip => server muammosi

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const http = axios.create({
-    baseURL: "http://backend.tcti.uz",
+    baseURL: "https://tktiga.herokuapp.com",
 });
 
 http.interceptors.request.use((config) => {
